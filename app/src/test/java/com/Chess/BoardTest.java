d439ea05a8eee39e0d45e03391b49adf013f4fc5
JongWoo Park <jongwoopark@JongWoos-MacBook-Pro.local>
2026-03-03 16:37:18 +0900

    Initialize boardTest
    Added a testcase strucuture for BoardTest

--- app/src/test/java/com/Chess/BoardTest.java
+++ app/src/test/java/com/Chess/BoardTest.java
@@ -1,4 +1,22 @@
 package com.Chess;
 
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
 public class BoardTest {
+
+    @Test
+    public void create() throws Exception{
+        Board newBoard = new Board();
+        Pawn whitePawn = new Pawn(Pawn.WHITE_COLOR);
+        newBoard.add(whitePawn);
+        assertThat(1).isEqualTo(newBoard.size());
+        assertThat(whitePawn).isEqualTo(newBoard.findPawn(0));
+
+        Pawn blackPawn = new Pawn(Pawn.BLACK_COLOR);
+        newBoard.add(blackPawn);
+        assertThat(2).isEqualTo(newBoard.size());
+        assertThat(blackPawn).isEqualTo(newBoard.findPawn(1));
+
+    }
 }
